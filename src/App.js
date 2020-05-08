2d75b80f2ff7b83acdc77e4fe054d653409fcef4
Zac Stowell <zgstowell@gmail.com>
2020-05-07 21:00:03 -0700

    pre deploy

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
 import ListContainer from './components/ListContainer';
-import SearchForm from './components/SearchForm'
-
 import './App.css';
 
 class App extends Component {
@@ -9,8 +7,11 @@ class App extends Component {
 
     return (
       <div className="container">
-        <div className="banner">
-          <h1>Employee Manager</h1>
+        <div className="jumbotron jumbotron-fluid banner">
+          <div className="container">
+            <h1 className="display-4">Employee Manager</h1>
+            <p className="lead">Below is a list of employees. Search by any property using the search field. Sort by Name or Location by clicking on the corresponding icons.</p>
+          </div>
         </div>
         <ListContainer />
       </div>
--- src/components/ListContainer/index.js
+++ src/components/ListContainer/index.js
@@ -6,8 +6,7 @@ import API from '../utils/API';
 export default class ListContainer extends Component {
     state = {
         employees: [],
-        filteredEmps: [],
-        search: ""
+        filteredEmps: []
     };
 
     componentDidMount() {
@@ -17,56 +16,70 @@ export default class ListContainer extends Component {
     }
 
     handleInputChange = event => {
-        const { name, value } = event.target;
-
-        this.setState({
-            [name]: value
+        const filter = event.target.value;
+        const filteredList = this.state.employees.filter(item => {
+            // merge all object into a single string and check if user input is within
+            let values = Object.values(item) // Object.values() turns the pass object's data into an array
+                .join("")// transform the array into string
+                .toLowerCase();// take away capitalization for searching
+            return values.indexOf(filter.toLowerCase()) !== -1; //search string and return filtered content
         });
-    };
+        this.setState({ filteredEmps: filteredList });
+    }
+
+    handleNameClick = () => {
+
+        const sorted = this.state.filteredEmps.sort((a, b) => a.name.first > b.name.first ? 1 : -1);
+        this.setState({ filteredEmps: sorted })
+    }
+
+    handleLocationClick = () => {
+        const sorted = this.state.filteredEmps.sort((a, b) => a.location.city > b.location.city ? 1 : -1);
+        this.setState({ filteredEmps: sorted })
+    }
+
 
     handleSubmit = event => {
         event.preventDefault();
 
-        console.log(this.state.search)
         const empFilter = this.state.employees.filter((employee) => {
-                const fullName = `${employee.name.first} ${employee.name.last}`;
-                return fullName.toLowerCase().includes(this.state.search.toLowerCase())
+            const fullName = `${employee.name.first} ${employee.name.last}`;
+            return fullName.toLowerCase().includes(this.state.search.toLowerCase())
 
         })
-        console.log(empFilter);
-        this.setState({filteredEmps: empFilter})
+        this.setState({ filteredEmps: empFilter })
         // need an array .filter() here? does this whole page need to be part of the
         // list container in order to have access to the array of employees?
     }
 
     render() {
         return (
-            <div>
+            <div className="list-container">
                 <form className="form-inline" id="form-center" onSubmit={this.handleSubmit}>
-                <div className="form-group mb-2">
-                    <label htmlFor="emp-search" className="sr-only">Search Employees: </label>
-                    <input
-                        type="text"
-                        name="search"
-                        onChange={this.handleInputChange}
-                        placeholder="Search by Name"
+                    <div className="form-group mb-2">
+                        <label htmlFor="emp-search" className="sr-only">Search Employees: </label>
+                        <input
+                            type="text"
+                            name="search"
+                            onChange={this.handleInputChange}
+                            placeholder="Search Employees"
                         />
-                </div>
+                    </div>
                     <button type="submit" className="btn btn-primary mb-2">Search!</button>
-            </form>
-                
+                </form>
+
                 <table className="table table-striped">
                     <thead>
                         <tr>
                             <th></th>
-                            <th scope="col">Name</th>
-                            <th scope="col">Email</th>
-                            <th scope="col">Location</th>
-                            <th scope="col">Cell Number</th>
+                            <th scope="col" onClick={this.handleNameClick} className="name.first"><i className="fas fa-user-alt center-icon"></i> <i className="fas fa-caret-down"></i></th>
+                            <th scope="col" className="center-icon"><i className="far fa-envelope center-icon"></i></th>
+                            <th scope="col" className="center-icon" onClick={this.handleLocationClick}><i className="fas fa-map-marked-alt center-icon"></i>  <i className="fas fa-caret-down"></i></th>
+                            <th scope="col" className="center-icon"><i className="fas fa-mobile-alt center-icon"></i></th>
                         </tr>
                     </thead>
                     <tbody>
-                        {this.state.filteredEmps.map((employee, index) => <EmployeeCard firstName={employee.name.first} lastName={employee.name.last} city={employee.location.city} state={employee.location.state} email={employee.email} cell={employee.cell} picURL={employee.picture.thumbnail} key={index}/>)}
+                        {this.state.filteredEmps.map((employee, index) => <EmployeeCard firstName={employee.name.first} lastName={employee.name.last} city={employee.location.city} state={employee.location.state} email={employee.email} cell={employee.cell} picURL={employee.picture.thumbnail} key={index} />)}
                     </tbody>
                 </table>
             </div>
